f9a848c51ddc864248ccaf4478aab29a8e02743a
Ömer Dikyol <omerdikyol02@gmail.com>
2023-12-25 19:41:28 +0300

    Update on sahibinden

--- sahibinden/ecommerce_app/models.py
+++ sahibinden/ecommerce_app/models.py
@@ -1,12 +1,12 @@
-# ecommerce_app/models.py
 from django.db import models
 
 class Product(models.Model):
     product_no = models.CharField(max_length=10)
-    description = models.TextField()
+    description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='products/')
     category = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product_no
+        return self.description
