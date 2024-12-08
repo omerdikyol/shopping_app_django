f8b2bc00b838777b750a76bd48c9925f0c652355
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2024-12-08 23:49:50 +0100

    Generate product number automatically
    Created the function to generate product number automatically

--- sahibinden/ecommerce_app/models.py
+++ sahibinden/ecommerce_app/models.py
@@ -12,3 +12,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.product_no:
+            last_product = Product.objects.all().order_by('product_no').last()
+            if last_product:
+                last_product_no = int(last_product.product_no[1:]) + 1
+                self.product_no = f"P{last_product_no:03d}"
+            else:
+                self.product_no = "P001"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+# to generate product number automatically 
